e1a3266f026c0a2c84e64f9e5fd1ec7fcd5156cf
alexandresouza9 <alexandresouza9@gmail.com>
2022-10-07 13:15:08 -0300

    commit

--- Exemplo_python.py
+++ Exemplo_python.py
@@ -3,3 +3,22 @@ print("Hello mundo!")
 print("Olá mundo!")
 
 print("Olá seja bem vindo a essa jornada de Python!")
+print(11 + 100 + 1000)
+print(1.5 + 1 + 0.5)
+print(True)
+print(False)
+print("Pythoon")
+
+
+nome = "Alexandre"
+idade = "40"
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ("RJ", "SP", "CE", "SC")
+
+BRAZILIAN_STATES = 10
+
+print(BRAZILIAN_STATES)
+
